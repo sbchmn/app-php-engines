2f848601ee42474efd9291e6be5dd635636c6e43
sbchmn <143567349+sbchmn@users.noreply.github.com>
2023-08-30 11:55:51 -0500

    Update info.php

--- deploy/info.php
+++ deploy/info.php
@@ -5,9 +5,10 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'php_engines';
-$app['version'] = '1.1.8';
+$app['version'] = '1.3.0';
+$app['release'] = '1';
 $app['vendor'] = 'WikiSuite';
-$app['packager'] = 'eGloo';
+$app['packager'] = 'ClearFoundation';
 $app['license'] = 'GPLv3';
 $app['license_core'] = 'LGPLv3';
 $app['description'] = lang('php_engines_app_description');
@@ -51,25 +52,8 @@ $app['core_requires'] = array(
     'app-web-server-core >= 1:2.4.0',
     'app-flexshare-core >= 1:2.4.10',
     'clearos-base >= 7.0.2',
-    'rh-php56-php-bcmath',
-    'rh-php56-php-cli',
-    'rh-php56-php-common',
-    'rh-php56-php-fpm',
-    'rh-php56-php-gd',
-    'rh-php56-php-intl',
-    'rh-php56-php-ldap',
-    'rh-php56-php-mbstring',
-    'rh-php56-php-pecl-memcache',
-    'rh-php56-php-mysqlnd',
-    'rh-php56-php-opcache',
-    'rh-php56-php-pdo',
-    'rh-php56-php-pear',
-    'rh-php56-php-soap',
-    'rh-php56-php-xml',
-    'rh-php56-php-xmlrpc',
+    'rh-php70',
     'rh-php70-php-bcmath',
-    'rh-php70-php-cli',
-    'rh-php70-php-common',
     'rh-php70-php-fpm',
     'rh-php70-php-gd',
     'rh-php70-php-intl',
@@ -78,27 +62,56 @@ $app['core_requires'] = array(
     'rh-php70-php-mysqlnd',
     'rh-php70-php-opcache',
     'rh-php70-php-pdo',
-    'rh-php70-php-pear',
     'rh-php70-php-soap',
-    'rh-php70-php-xml',
+    'rh-php71',
     'rh-php71-php-bcmath',
-    'rh-php71-php-cli',
-    'rh-php71-php-common',
     'rh-php71-php-fpm',
     'rh-php71-php-gd',
     'rh-php71-php-gmp',
     'rh-php71-php-intl',
-    'rh-php71-php-json',
     'rh-php71-php-ldap',
     'rh-php71-php-mbstring',
     'rh-php71-php-mysqlnd',
     'rh-php71-php-opcache',
     'rh-php71-php-pdo',
-    'rh-php71-php-pear',
-    'rh-php71-php-process',
     'rh-php71-php-soap',
-    'rh-php71-php-xml',
-    'rh-php71-php-zip',
+    'rh-php72',
+    'rh-php72-php-bcmath',
+    'rh-php72-php-fpm',
+    'rh-php72-php-gd',
+    'rh-php72-php-gmp',
+    'rh-php72-php-intl',
+    'rh-php72-php-ldap',
+    'rh-php72-php-mbstring',
+    'rh-php72-php-mysqlnd',
+    'rh-php72-php-opcache',
+    'rh-php72-php-pdo',
+    'rh-php72-php-soap',
+    'rh-php73',
+    'rh-php73-php-bcmath',
+    'rh-php73-php-fpm',
+    'rh-php73-php-gd',
+    'rh-php73-php-gmp',
+    'rh-php73-php-intl',
+    'rh-php73-php-ldap',
+    'rh-php73-php-mbstring',
+    'rh-php73-php-mysqlnd',
+    'rh-php73-php-opcache',
+    'rh-php73-php-pdo',
+    'rh-php73-php-soap',
+    'php74',
+    'php74-php-bcmath',
+    'php74-php-fpm',
+    'php74-php-gd',
+    'php74-php-gmp',
+    'php74-php-intl',
+    'php74-php-ldap',
+    'php74-php-mbstring',
+    'php74-php-mysqlnd',
+    'php74-php-opcache',
+    'php74-php-pdo',
+    'php74-php-soap',
+
 );
 
 $app['core_directory_manifest'] = array(
@@ -108,9 +121,11 @@ $app['core_directory_manifest'] = array(
 );
 
 $app['core_file_manifest'] = array(
-    'rh-php56-php-fpm.php' => array('target' => '/var/clearos/base/daemon/rh-php56-php-fpm.php'),
     'rh-php70-php-fpm.php' => array('target' => '/var/clearos/base/daemon/rh-php70-php-fpm.php'),
     'rh-php71-php-fpm.php' => array('target' => '/var/clearos/base/daemon/rh-php71-php-fpm.php'),
+    'rh-php72-php-fpm.php' => array('target' => '/var/clearos/base/daemon/rh-php72-php-fpm.php'),
+    'rh-php73-php-fpm.php' => array('target' => '/var/clearos/base/daemon/rh-php73-php-fpm.php'),
+    'rh-php74-php-fpm.php' => array('target' => '/var/clearos/base/daemon/rh-php74-php-fpm.php'),
     'apache-php_engines_path.conf' => array(
         'target' => '/etc/httpd/conf.d/php_engines_path.conf',
         'config' => TRUE,
@@ -131,9 +146,12 @@ $app['core_file_manifest'] = array(
     ),
     'www_path.conf' => array(
         'target' => [
-            '/etc/opt/rh/rh-php56/php-fpm.d/www_path.conf',
             '/etc/opt/rh/rh-php70/php-fpm.d/www_path.conf',
-            '/etc/opt/rh/rh-php71/php-fpm.d/www_path.conf'
+            '/etc/opt/rh/rh-php71/php-fpm.d/www_path.conf',
+            '/etc/opt/rh/rh-php72/php-fpm.d/www_path.conf',
+            '/etc/opt/rh/rh-php73/php-fpm.d/www_path.conf',
+            '/etc/opt/rh/rh-php74/php-fpm.d/www_path.conf',
+
         ],
         'config' => TRUE,
         'config_params' => 'noreplace',
