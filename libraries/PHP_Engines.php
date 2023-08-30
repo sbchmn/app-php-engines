0a6e549763918f703dd7d2d06617dee28ef1f0d9
sbchmn <143567349+sbchmn@users.noreply.github.com>
2023-08-30 11:09:24 -0500

    Update PHP_Engines.php
    Origin for USE Remi PHP

--- libraries/PHP_Engines.php
+++ libraries/PHP_Engines.php
@@ -107,26 +107,43 @@ class PHP_Engines extends Engine
         'httpd' => 'PHP 5.4',
         'rh-php56-php-fpm' => 'PHP 5.6',
         'rh-php70-php-fpm' => 'PHP 7.0',
-        'rh-php71-php-fpm' => 'PHP 7.1'
+        'rh-php71-php-fpm' => 'PHP 7.1',
+        'rh-php72-php-fpm' => 'PHP 7.2',
+        'rh-php73-php-fpm' => 'PHP 7.3',
+        'rh-php74-php-fpm' => 'PHP 7.4',
+        'rh-php83-php-fpm' => 'PHP 8.3'
     );
 
     protected $ports = array(
         'httpd' => 0,
         'rh-php56-php-fpm' => 9056,
         'rh-php70-php-fpm' => 9070,
-        'rh-php71-php-fpm' => 9071
+        'rh-php71-php-fpm' => 9071,
+        'rh-php72-php-fpm' => 9072,
+        'rh-php73-php-fpm' => 9073,
+        'rh-php74-php-fpm' => 9074,
+        'rh-php83-php-fpm' => 9083
     );
 
     protected $version_codes = array(
         'rh-php56-php-fpm' => 56,
         'rh-php70-php-fpm' => 70,
         'rh-php71-php-fpm' => 71
+        'rh-php72-php-fpm' => 72
+        'rh-php73-php-fpm' => 73
+        'rh-php74-php-fpm' => 74
+        'rh-php83-php-fpm' => 83
+        
     );
 
     protected $configs = array(
         'rh-php56-php-fpm' => '/etc/opt/rh/rh-php56/php.ini',
         'rh-php70-php-fpm' => '/etc/opt/rh/rh-php70/php.ini',
         'rh-php71-php-fpm' => '/etc/opt/rh/rh-php71/php.ini'
+        'rh-php72-php-fpm' => '/etc/opt/rh/rh-php72/php.ini'
+        'rh-php73-php-fpm' => '/etc/opt/rh/rh-php73/php.ini'
+        'rh-php74-php-fpm' => '/etc/opt/rh/rh-php74/php.ini'
+        'rh-php83-php-fpm' => '/etc/opt/rh/rh-php83/php.ini'
     );
 
     const FILE_APP_CONFIG = '/etc/clearos/php_engines.conf';
